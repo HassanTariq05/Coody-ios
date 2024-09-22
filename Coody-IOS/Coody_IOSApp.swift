2e1c8bd56c602faf65e98158a3069bca04994b22
Hassan Tariq <hassan.tarique05@gmail.com>
2024-09-22 13:19:13 +0500

    Initial Commit

---
+++ Coody-IOS/Coody_IOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  Coody_IOSApp.swift
+//  Coody-IOS
+//
+//  Created by Hassan Tariq on 22/09/2024.
+//
+
+import SwiftUI
+
+@main
+struct Coody_IOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
